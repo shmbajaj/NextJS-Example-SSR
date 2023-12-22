e5a221c5ef9cdd47f390b7a53abfe9df6431efbf
Sahil Suman <sahilsuman933@gmail.com>
2023-12-22 20:40:42 +0530

    updated error message.
    Signed-off-by: Sahil Suman <sahilsuman933@gmail.com>

--- app/user/route.ts
+++ app/user/route.ts
@@ -18,18 +18,7 @@ export async function GET(request: Request) {
            return new Response(JSON.stringify(user), {status: 200, headers: {'Content-Type': 'application/json'}});
         }
     } catch (error : any) {
-        try {
-            const {detail} = JSON.parse(error.message.split('\n').pop());
-
-            if (detail === 'Token Expired - token expired') {
-                return new Response('Token expired', {status: 401});
-            }
-        } catch (e) {
-            console.log('Error parsing response:', e);
-        }
-
-        console.log("LOG:", error);
-        return new Response('Internal server error', {status: 500});
+        return new Response(error, {status: 401});
     }
 
 
